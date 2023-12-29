3697d0c73b4acc22974eabfa2249f219787119d7
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-12-29 21:53:50 +0800

    fix2 fetch

--- github-projects.js
+++ github-projects.js
@@ -1,29 +1,27 @@
-const fetch = require('node-fetch').default;
-// GitHub API script
-const apiUrl = 'https://api.github.com/users/BDhaCkers009/repos';
-const token = process.env.PROJECTS_TOKEN;
+import fetch from 'node-fetch';
+
+const apiUrl = 'https://api.github.com/users/BDhaCkers009/repos'; // Update with your actual GitHub username
+const token = process.env.PROJECTS_TOKEN; // Update to the correct environment variable name
 
 fetch(apiUrl, {
     headers: {
         Authorization: `Bearer ${token}`,
     },
 })
-.then(response => response.json())
-.then(data => {
-    const projectsContainer = document.getElementById('projects-container');
+    .then(response => response.json())
+    .then(data => {
+        const projectsContainer = document.getElementById('projects-container');
 
-    data.forEach(project => {
-        // Create a project card
-        const projectCard = document.createElement('div');
-        projectCard.className = 'project-card p-6 rounded-md';
-        projectCard.innerHTML = `
-            <h3 class="text-xl mb-4">${project.name}</h3>
-            <p class="text-gray-400 mb-4">${project.description || 'No description available.'}</p>
-            <a href="${project.html_url}" class="text-green-500 hover:underline">View on GitHub</a>
-        `;
+        data.forEach(project => {
+            const projectCard = document.createElement('div');
+            projectCard.className = 'project-card p-6 rounded-md';
+            projectCard.innerHTML = `
+                <h3 class="text-xl mb-4">${project.name}</h3>
+                <p class="text-gray-400 mb-4">${project.description || 'No description available.'}</p>
+                <a href="${project.html_url}" class="text-green-500 hover:underline">View on GitHub</a>
+            `;
 
-        // Append the project card to the container
-        projectsContainer.appendChild(projectCard);
-    });
-})
-.catch(error => console.error('Error fetching GitHub projects:', error));
+            projectsContainer.appendChild(projectCard);
+        });
+    })
+    .catch(error => console.error('Error fetching GitHub projects:', error));
